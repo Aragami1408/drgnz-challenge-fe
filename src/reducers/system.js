06956e77cb46ce032abeb989a0bbed5570e0db4c
Drgnz <12205347+drgnz@users.noreply.github.com>
2018-12-27 02:06:41 +0700

    Fix issue with signup

--- src/reducers/system.js
+++ src/reducers/system.js
@@ -1,6 +1,7 @@
 import {
   VALIDATE_TOKEN_SUCCESS,
   LOGIN_SUCCESS,
+  SIGNUP_SUCCESS,
 } from './auth';
 
 const initialState = {
@@ -10,6 +11,7 @@ const initialState = {
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case VALIDATE_TOKEN_SUCCESS:
+    case SIGNUP_SUCCESS:
     case LOGIN_SUCCESS: {
       return {
         isLoading: false,
